87d6158bde90a28883e35cd82230251ce78f58d3
Sat <satriasauraly@gmail.com>
2024-07-20 20:38:11 +0700

    My branch -Stskmar

--- src/components/layout/Review.jsx
+++ src/components/layout/Review.jsx
@@ -8,7 +8,7 @@ export default function Review() {
       <div className="flex flex-col items-center justify-center text-center ">
         <div className="space-y-2">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-            Testimoni
+            Testimoniaa
           </h2>
           <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
             Lihat apa yang mereka katakan.
